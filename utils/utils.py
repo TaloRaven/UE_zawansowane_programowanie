f34aa9cde42584df94f25f5ffe79e0773dcffcd9
MaciekG <maciek.1998.g@gmail.com>
2021-11-23 21:21:58 +0100

    lab_8

---
+++ utils/utils.py
@@ -0,0 +1,16 @@
+import csv
+import json
+from json import JSONEncoder
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+def read(csv_name: str):
+    csvfile = open('./data/{}.csv'.format(csv_name), 'r', encoding='utf-8')
+    for i in csvfile:
+        filename = i.strip()
+        break
+    filenames = filename.split(',')
+    reader = csv.DictReader(csvfile, filenames)
+    return reader
