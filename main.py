2b19deac8a86a1825d09d4495d4d0677a2dfb6c8
MaciejGardziński <49470197+TaloRaven@users.noreply.github.com>
2022-01-18 14:46:10 +0100

    Update main.py
    glob

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import cv2
 import imutils
 import numpy as np
 from Modules.Persondetection import person_detect
+import glob
+
+
+def main():
+    cv_img = []
+    for img in glob.glob("img/*.jpg"):
+        n = cv2.imread(img)
+        cv_img.append(n)
+
+    for x in cv_img:
+        person_detect(x)
 
 
 if __name__ == "__main__":
-    person_detect('1.jpg')
-    person_detect('2.jpg')
-    person_detect('3.jpg')
-    person_detect('4.jpg')
+    main()
